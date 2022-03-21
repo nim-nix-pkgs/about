cee2f2c7784c98d7ed56591c6c9f6a762b29b9c2
hugosenari <hugosenari@gmail.com>
2022-03-21 19:52:40 -0300

    chore: re-index ../nimpkgs/a/about/v0_1_1

--- v0_1_1/flake.nix
+++ v0_1_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-about-v0_1_1.flake = false;
   inputs.src-about-v0_1_1.owner = "aleandros";
-  inputs.src-about-v0_1_1.ref   = "refs/tags/v0.1.1";
+  inputs.src-about-v0_1_1.ref   = "v0_1_1";
   inputs.src-about-v0_1_1.repo  = "about";
   inputs.src-about-v0_1_1.type  = "github";
   
